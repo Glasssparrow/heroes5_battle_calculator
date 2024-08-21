f16ec18254a7ee305c074bfdb9c630df7ad0f453
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-08-21 21:39:14 +0300

    sketch

--- tests/tournament_order.py
+++ tests/tournament_order.py
@@ -2,9 +2,10 @@ from tournament_manager.duel import duel
 
 
 def tournament_order():
-    duel(
+    winner = duel(
         "Манекен1",
         "Манекен2",
         100, 100,
-        "Количество"
+        0
     )
+    print(winner)
--- tournament_manager/duel.py
+++ tournament_manager/duel.py
@@ -7,9 +7,18 @@ def duel(
         second_unit_quantity: int,
         type_of_quantity: int,
 ):
+    """
+    :param first_unit_name: name for database
+    :param second_unit_name: name for database
+    :param first_unit_quantity: of soldiers/gold/weeks
+    :param second_unit_quantity: of soldiers/gold/weeks
+    :param type_of_quantity: 0: quantity
+    :return: name of winner
+    """
     print(
         f"Duel!\n"
         f"{first_unit_name}x{first_unit_quantity}vs"
         f"{second_unit_name}x{second_unit_quantity}.\n"
         f"Type of duel - {type_of_quantity}."
     )
+    return "Duel is fake!"
