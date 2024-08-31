49497baa687e10211dab0ebeafd3ce3937be8950
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-08-31 23:09:14 +0300

    sketch

--- simulator/turn/turm_simulator.py
+++ simulator/turn/turm_simulator.py
@@ -4,7 +4,8 @@ from ..battle_map.battle_map import BattleMap
 class TurnSimulator:
 
     def __init__(self, units: list, battle_map: BattleMap):
-        pass
+        self.units = units
+        self.map = battle_map
 
     def next_turn(self, active_unit):
         pass
