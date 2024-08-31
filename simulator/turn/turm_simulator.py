fdc388ee779ab61f3e9948583ac02a0b327a43c1
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-08-31 23:31:22 +0300

    sketch

--- simulator/turn/turm_simulator.py
+++ simulator/turn/turm_simulator.py
@@ -6,6 +6,16 @@ class TurnSimulator:
     def __init__(self, units: list, battle_map: BattleMap):
         self.units = units
         self.map = battle_map
+        self.map.add_unit(
+            unit=self.units[0],
+            x=0, y=5,
+            color=self.units[0].color,
+        )
+        self.map.add_unit(
+            unit=self.units[0],
+            x=11, y=5,
+            color=self.units[0].color,
+        )
 
     def next_turn(self, active_unit):
         pass
