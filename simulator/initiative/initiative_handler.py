bbc3d9ceec1e9a8d4046f00a4eb9b5fb09fb9810
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-08-31 12:54:16 +0300

    sketch

--- simulator/initiative/initiative_handler.py
+++ simulator/initiative/initiative_handler.py
@@ -4,3 +4,12 @@ class Initiative:
 
     def __init__(self, units: list):
         self.units = units
+        self.active_unit_index = -1
+
+    def get_active_unit(self):
+        self.active_unit_index += 1
+        if self.active_unit_index < len(self.units):
+            return self.units[self.active_unit_index]
+        else:
+            self.active_unit_index = 0
+            return self.units[self.active_unit_index]
