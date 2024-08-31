4ff2fe67994ff29ff5cc7f1872d74990e16da348
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-08-31 13:08:31 +0300

    sketch

--- simulator/battle.py
+++ simulator/battle.py
@@ -1,4 +1,7 @@
+from .initiative.initiative_handler import Initiative
 
 
 def battle(unit1, unit2):
+    initiative_system = Initiative([unit1, unit2])
+    active_unit = initiative_system.get_active_unit()
     return unit1
