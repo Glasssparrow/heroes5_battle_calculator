0ba426a8f4d0c45fe9dc5976cb1f8d54a1efff3a
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-08-31 23:20:38 +0300

    nothing
    some whitespace

--- read_database/read_database.py
+++ read_database/read_database.py
@@ -5,10 +5,10 @@ class DataBase:
 
     def __init__(self):
         self.for_unit1 = {
-            "name":"Пугало",
-            "attack":1, "defence":1, "min_damage":1, "max_damage":1,
-            "health":1000, "initiative":8, "speed":4,
-            "mana":0, "ammo":0,
+            "name": "Пугало",
+            "attack": 1, "defence": 1, "min_damage": 1, "max_damage": 1,
+            "health": 1000, "initiative": 8, "speed": 4,
+            "mana": 0, "ammo": 0,
         }
 
     def get_unit(self, unit_name):
