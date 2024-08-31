a100095398a9c37d2034efe32c47264f818519b1
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-08-31 14:50:44 +0300

    sketch

--- simulator/battle.py
+++ simulator/battle.py
@@ -2,7 +2,8 @@ from .initiative.initiative_handler import Initiative
 
 
 def battle(unit1, unit2, turn_limit):
-    initiative_system = Initiative([unit1, unit2])
+    units = [unit1, unit2]
+    initiative_system = Initiative(units)
     for turn in range(turn_limit):
         active_unit = initiative_system.get_active_unit()
     return unit1
