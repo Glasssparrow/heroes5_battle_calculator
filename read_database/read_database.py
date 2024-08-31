f2a7c286de6c07b43fc7dd6ec87d60b23443323a
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-08-31 23:13:45 +0300

    sketch

--- read_database/read_database.py
+++ read_database/read_database.py
@@ -6,7 +6,6 @@ class DataBase:
     def __init__(self):
         self.the_unit = Unit(
             name="Пугало",
-            color="Синий",
             attack=1, defence=1, min_damage=1, max_damage=1,
             health=1000, initiative=8, speed=4,
             mana=0, ammo=0,
