a6fc49b6f796af275d4d5ef4e1e20c3842e0f518
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-04 23:33:58 +0300

    sketch

--- simulator/dicision_maker/choose_action.py
+++ simulator/dicision_maker/choose_action.py
@@ -1,4 +1,9 @@
+from .get_positions import (
+    get_melee_attack_positions,
+    get_range_attack_positions
+)
 
 
 def choose_action():
-    pass
+    melee_attacks = get_melee_attack_positions()
+    range_attacks = get_range_attack_positions()
