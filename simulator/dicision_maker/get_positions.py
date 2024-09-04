7463b16a720743e7dd82e28822be08dc67859f87
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-04 23:31:14 +0300

    sketch

--- simulator/dicision_maker/get_positions.py
+++ simulator/dicision_maker/get_positions.py
@@ -5,6 +5,7 @@ class AttackPositions:
     def __init__(self):
         self.positions = []
         self.action_name = None
+        self.action_threat_lvl = 0
 
 
 def get_melee_attack_positions():
