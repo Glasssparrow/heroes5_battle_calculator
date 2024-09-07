da9fb0383300c483218bb90b235311587337dc1e
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-07 21:14:22 +0300

    Update get_positions.py

--- simulator/dicision_maker/get_positions.py
+++ simulator/dicision_maker/get_positions.py
@@ -9,12 +9,16 @@ class AttackPositions:
         self.action_threat_lvl = action.threat
 
 
-def get_melee_attack_positions(unit, battle_map):
+def get_melee_attack_positions(the_unit, battle_map):
     attack_positions = []
-    available_cells = battle_map.get_available_cells(unit)
+    available_cells = battle_map.get_available_cells(the_unit)
+    enemy_units = []
+    for unit in battle_map.units:
+        if unit.color != the_unit.color:
+            enemy_units.append(unit)
     for (x, y), length, path in available_cells:
         pass
-    for action_index, action in enumerate(unit.actions):
+    for action_index, action in enumerate(the_unit.actions):
         attack_positions.append(AttackPositions(
             action_index=action_index,
             action=action,
