d1abb42d5d845c546f5795ff139af186c343c089
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-07 13:35:55 +0300

    sketch

--- read_database/read_database.py
+++ read_database/read_database.py
@@ -7,12 +7,21 @@ class DataBase:
         self.for_unit1 = {
             "name": "Пугало",
             "attack": 1, "defence": 1, "min_damage": 1, "max_damage": 1,
-            "health": 1000, "initiative": 8, "speed": 4,
+            "health": 50, "initiative": 8, "speed": 4,
+            "mana": 0, "ammo": 0,
+        }
+        self.for_unit2 = {
+            "name": "Манекен",
+            "attack": 1, "defence": 1, "min_damage": 1, "max_damage": 1,
+            "health": 50, "initiative": 8, "speed": 4,
             "mana": 0, "ammo": 0,
         }
 
     def get_unit(self, unit_name):
-        the_unit = Unit(**self.for_unit1)
+        if unit_name == "Манекен":
+            the_unit = Unit(**self.for_unit1)
+        else:
+            the_unit = Unit(**self.for_unit2)
         the_unit.add_action(Melee)
         return the_unit
 
--- tests/tournament_order.py
+++ tests/tournament_order.py
@@ -3,11 +3,11 @@ from tournament_manager.just_1vs1 import just_1vs1
 
 def tournament_order():
     winner = just_1vs1(
-        "Манекен1",
-        "Манекен2",
+        "Манекен",
+        "Пугало",
         100, 100,
         0,
         1,
-        3,
+        5,
     )
     print(f'Манекен1 have {winner*100}% chance to win')
