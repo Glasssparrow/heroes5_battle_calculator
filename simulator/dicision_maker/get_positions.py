f1718f6e88eab32008ff0ec71e8cc4c94f565396
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-07 14:54:04 +0300

    sketch

--- simulator/dicision_maker/get_positions.py
+++ simulator/dicision_maker/get_positions.py
@@ -5,6 +5,7 @@ class AttackPositions:
     def __init__(self):
         self.positions = []
         self.action_name = None
+        self.action_index = None  # position in unit's actions list.
         self.action_threat_lvl = 0
 
 
