37336adf97d87de467c9d31bb6420f569473f238
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-07 15:17:51 +0300

    sketch

--- simulator/dicision_maker/get_positions.py
+++ simulator/dicision_maker/get_positions.py
@@ -2,15 +2,20 @@
 
 class AttackPositions:
 
-    def __init__(self):
+    def __init__(self, action_index, action):
         self.positions = []
-        self.action_name = None
-        self.action_index = None  # position in unit's actions list.
-        self.action_threat_lvl = 0
+        self.action_name = action.name
+        self.action_index = action_index  # position in unit's actions list.
+        self.action_threat_lvl = action.threat
 
 
 def get_melee_attack_positions(unit, battle_map):
     attack_positions = []
+    for action_index, action in enumerate(unit.actions):
+        attack_positions.append(AttackPositions(
+            action_index=action_index,
+            action=action,
+        ))
 
 
 def get_range_attack_positions(unit, battle_map):
