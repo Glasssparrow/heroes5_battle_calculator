a0e488334c14148bc6405f83405dc5a086f3aa89
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-07 21:12:05 +0300

    Update get_positions.py

--- simulator/dicision_maker/get_positions.py
+++ simulator/dicision_maker/get_positions.py
@@ -12,6 +12,8 @@ class AttackPositions:
 def get_melee_attack_positions(unit, battle_map):
     attack_positions = []
     available_cells = battle_map.get_available_cells(unit)
+    for (x, y), length, path in available_cells:
+        pass
     for action_index, action in enumerate(unit.actions):
         attack_positions.append(AttackPositions(
             action_index=action_index,
