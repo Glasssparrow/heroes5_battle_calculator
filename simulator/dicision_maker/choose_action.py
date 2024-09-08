771adbc08a2edb0b140ae34c232f211339691b45
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-08 15:07:45 +0300

    Update choose_action.py

--- simulator/dicision_maker/choose_action.py
+++ simulator/dicision_maker/choose_action.py
@@ -10,7 +10,7 @@ def choose_action(the_unit, battle_map):
     enemy_units = get_hostile_units(the_unit, battle_map)
     melee_attacks = get_melee_attack_positions(the_unit, battle_map)
     range_attacks = get_range_attack_positions(the_unit, battle_map)
-    danger_zone = get_danger_zone(the_unit, battle_map)
+    danger_zone = get_danger_zone(enemy_units[0], battle_map)
     if not melee_attacks and not range_attacks:
         pass
     return 0
