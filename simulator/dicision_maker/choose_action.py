0d2eafeb0db3466f7bc7488d215417ee1a762192
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-08 13:45:54 +0300

    Update choose_action.py

--- simulator/dicision_maker/choose_action.py
+++ simulator/dicision_maker/choose_action.py
@@ -3,9 +3,11 @@ from .get_positions import (
     get_range_attack_positions
 )
 from .get_danger_zone import get_danger_zone
+from .common import get_hostile_units
 
 
 def choose_action(the_unit, battle_map):
+    enemy_units = get_hostile_units(the_unit, battle_map)
     melee_attacks = get_melee_attack_positions(the_unit, battle_map)
     range_attacks = get_range_attack_positions(the_unit, battle_map)
     danger_zone = get_danger_zone(the_unit, battle_map)
