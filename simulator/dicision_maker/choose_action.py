4712bf220621e4ab8c17ea8b2f0e2fa87685931f
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-20 22:04:13 +0300

    Update choose_action.py

--- simulator/dicision_maker/choose_action.py
+++ simulator/dicision_maker/choose_action.py
@@ -24,6 +24,8 @@ def choose_action(the_unit, battle_map):
         if attack_position.action.threat > threat:
             action = attack_position.action_index
             threat = attack_position.action.threat
+    # Выбор между атакой и выходом из опасной зоны.
+    # TODO
     danger_zone = get_danger_zone(enemy_units[0], battle_map)
     if not melee_attacks and not range_attacks:
         # Если не может атаковать, выбрать клетку
