709601114f078c65e897a40924cd5c5b5f09d84c
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-20 22:03:10 +0300

    Update choose_action.py

--- simulator/dicision_maker/choose_action.py
+++ simulator/dicision_maker/choose_action.py
@@ -10,6 +10,7 @@ def choose_action(the_unit, battle_map):
     enemy_units = get_hostile_units(the_unit, battle_map)
     melee_attacks = get_melee_attack_positions(the_unit, battle_map)
     range_attacks = get_range_attack_positions(the_unit, battle_map)
+    # Выбор действия с наибольшей угрозой.
     action = None  # Номер действия в листе действий экземпляра Unit
     threat = 0
     for attack_position in melee_attacks:
@@ -25,5 +26,7 @@ def choose_action(the_unit, battle_map):
             threat = attack_position.action.threat
     danger_zone = get_danger_zone(enemy_units[0], battle_map)
     if not melee_attacks and not range_attacks:
+        # Если не может атаковать, выбрать клетку
+        # для перемещения.
         pass
     return 0
