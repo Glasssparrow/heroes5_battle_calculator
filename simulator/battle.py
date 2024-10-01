d068c7b44d086e783baecba6508dc8d5c303f858
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-10-01 16:44:59 +0300

    comments

--- simulator/battle.py
+++ simulator/battle.py
@@ -6,6 +6,7 @@ from .battle_map.battle_map import BattleMap
 def battle(unit1, unit2, turn_limit):
     units = [unit1, unit2]
     initiative_system = Initiative(units)
+    # создаем карту и размещаем на ней юниты
     battle_map = BattleMap(10, 12)
     battle_map.add_unit(
         unit=unit1,
@@ -17,8 +18,10 @@ def battle(unit1, unit2, turn_limit):
         x=8, y=8,
         color="Красный",
     )
-    battle_map.create_pathfinders()
+    battle_map.create_pathfinders()  # Подготавливаем карту к работе.
+    # Подготавливаем симулятор
     simulation = TurnSimulator(units, battle_map)
+    # Расчитываем заданное количество ходов.
     for turn in range(turn_limit):
         active_unit = initiative_system.get_active_unit()
         simulation.next_turn(active_unit)
