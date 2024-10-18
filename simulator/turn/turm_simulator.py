1d36374eb19c245d68620a3f52c30e078019b707
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-10-18 23:03:44 +0300

    Update turm_simulator.py

--- simulator/turn/turm_simulator.py
+++ simulator/turn/turm_simulator.py
@@ -13,12 +13,12 @@ class TurnSimulator:
         active_unit.start_turn()
         # Выбираем действие с наибольшим уровнем угрозы,
         # которое можем применить.
-        action_index = choose_action(active_unit, self.map)
-        action = active_unit.actions[action_index]
+        decision = choose_action(active_unit, self.map)
+        action = active_unit.actions[decision]
         # Т.к. юнита только 2, а действия пока только ближнего боя,
         # цель это всегда второй юнит.
         # Ну или None, если действие это просто движение.
         target = choose_target(active_unit, action, self.map)
         if target:
-            active_unit.take_action(action_index, target, self.map)
+            active_unit.take_action(decision, target, self.map)
         active_unit.end_turn()
