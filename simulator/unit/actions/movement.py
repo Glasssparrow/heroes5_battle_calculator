78d4aa7c34b06e6b8055a567da314e5f773883f8
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-12-20 17:49:09 +0300

    Update movement.py

--- simulator/unit/actions/movement.py
+++ simulator/unit/actions/movement.py
@@ -1,7 +1,9 @@
 from .common import Action
+from ...simulator_keywords import JUST_MOVEMENT
 
 
 class Movement(Action):
 
     def __init__(self, owner):
         super().__init__(owner)
+        self.type_of_action = JUST_MOVEMENT
